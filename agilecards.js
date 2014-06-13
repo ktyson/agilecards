7ff434bbe9803d265e8429c2e23548867605807f
Karl <tyson.karl@gmail.com>
2014-06-12 19:22:12 -0500

    first load

---
+++ agilecards.js
@@ -0,0 +1,339 @@
+var cards = {
+ "Cards": [
+  {
+   "id": "1",
+   "area": "Thinking",
+   "practice": "Pair Programming", 
+   "slogan": "We help each other succeed.",
+   "allies": "",
+   "involves": "P",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "2",
+   "area": "Thinking",
+   "practice": "Energized Work",
+   "slogan": "We work at a pace that allows us to do our best, most productive work indefinitely.",
+   "allies": "",
+   "involves": "cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "3",
+   "area": "Thinking",
+   "practice": "Informative Workspace",
+   "slogan": "We are tuned in to the status of our project.",
+   "allies": "",
+   "involves": "cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "4",
+   "area": "Thinking",
+   "practice": "Root-Cause Analysis",
+   "slogan": "We prevent mistakes by fixing our process.",
+   "allies": "",
+   "involves": "cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "5",
+   "area": "Thinking",
+   "practice": "Retrospectives",
+   "slogan": "We continually improve our work habits.",
+   "allies": "",
+   "involves": "cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "6",
+   "area": "Collaborating",
+   "practice": "Trust",
+   "slogan": "We work together effectively and without fear.",
+   "allies": "",
+   "involves": "cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "7",
+   "area": "Collaborating",
+   "practice": "Sit Together",
+   "slogan": "We communicate rapidly and accurately.",
+   "allies": "",
+   "involves": "cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "8",
+   "area": "Collaborating",
+   "practice": "Real Customer Involvement",
+   "slogan": "We understand the goals and frustrations of our customers and end-users.",
+   "allies": "",
+   "involves": "c",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "9",
+   "area": "Collaborating",
+   "practice": "Ubiquitous Language",
+   "slogan": "We understand each other.",
+   "allies": "",
+   "involves": "P",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "10",
+   "area": "Collaborating",
+   "practice": "Stand-Up Meetings",
+   "slogan": "We know what our teammates are doing.",
+   "allies": "",
+   "involves": "cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "11",
+   "area": "Collaborating",
+   "practice": "Coding Standards",
+   "slogan": "We embrace a joint aesthetic.",
+   "allies": "",
+   "involves": "p",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "12",
+   "area": "Collaborating",
+   "practice": "Iteration Demo",
+   "slogan": "We keep it real.",
+   "allies": "",
+   "involves": "cpT",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "13",
+   "area": "Collaborating",
+   "practice": "Reporting",
+   "slogan": "We inspire trust in the team's decisions.",
+   "allies": "",
+   "involves": "cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "14",
+   "area": "Releasing",
+   "practice": "Done Done",
+   "slogan": "We're done when we're production-ready.",
+   "allies": "",
+   "involves": "CPT",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "15",
+   "area": "Releasing",
+   "practice": "No Bugs",
+   "slogan": "We confidently release without a dedicated testing phase.",
+   "allies": "",
+   "involves": "Cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "16",
+   "area": "Releasing",
+   "practice": "Version Control",
+   "slogan": "We keep all of our project artifacts in a single, authoritative place.",
+   "allies": "",
+   "involves": "cPt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "17",
+   "area": "Releasing",
+   "practice": "Ten-Minute Build",
+   "slogan": "We eliminate build and configuration hassles.",
+   "allies": "",
+   "involves": "P",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "18",
+   "area": "Releasing",
+   "practice": "Continuous Integration",
+   "slogan": "We keep our code ready to ship.",
+   "allies": "",
+   "involves": "P",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "19",
+   "area": "Releasing",
+   "practice": "Collective Code Ownership",
+   "slogan": "We are all responsible for high quality code.",
+   "allies": "",
+   "involves": "P",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "20",
+   "area": "Releasing",
+   "practice": "Documentation",
+   "slogan": "We communicate necessary information effectively.",
+   "allies": "",
+   "involves": "cp",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "21",
+   "area": "Planning",
+   "practice": "Vision",
+   "slogan": "We know why our work is important and how we'll be successful.",
+   "allies": "",
+   "involves": "C",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "22",
+   "area": "Planning",
+   "practice": "Release Planning",
+   "slogan": "We plan for success.",
+   "allies": "",
+   "involves": "C",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "23",
+   "area": "Planning",
+   "practice": "The Planning Game",
+   "slogan": "Our plans take advantage of both business and technology expertise.",
+   "allies": "",
+   "involves": "cp",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "24",
+   "area": "Planning",
+   "practice": "Risk Management",
+   "slogan": "We make and meet long term commitments",
+   "allies": "",
+   "involves": "cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "25",
+   "area": "Planning",
+   "practice": "Iteration Planning",
+   "slogan": "We stop at predetermined, unchangeable time intervals and compare reality to plan.",
+   "allies": "",
+   "involves": "cP",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "26",
+   "area": "Planning",
+   "practice": "Slack",
+   "slogan": "We deliver on our iteration commitments.",
+   "allies": "",
+   "involves": "p",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "27",
+   "area": "Planning",
+   "practice": "Stories",
+   "slogan": "We plan our work in small, customer-centric pieces.",
+   "allies": "",
+   "involves": "cpt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "28",
+   "area": "Planning",
+   "practice": "Estimating",
+   "slogan": "We provide reliable estimates.",
+   "allies": "",
+   "involves": "P",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "29",
+   "area": "Developing",
+   "practice": "Incremental Requirements",
+   "slogan": "We define requirements in parallel with other work.",
+   "allies": "",
+   "involves": "CT",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "30",
+   "area": "Developing",
+   "practice": "Customer Tests",
+   "slogan": "We implement tricky domain concepts correctly.",
+   "allies": "",
+   "involves": "CPT",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "31",
+   "area": "Developing",
+   "practice": "Test-Driven Development",
+   "slogan": "We produce well-designed, well-tested, and well-factored code in small, verifiable steps.",
+   "allies": "",
+   "involves": "Pt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "32",
+   "area": "Developing",
+   "practice": "Refactoring",
+   "slogan": "Every day, our code is slightly better than it was the day before.",
+   "allies": "",
+   "involves": "Pt",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "33",
+   "area": "Developing",
+   "practice": "Simple Design",
+   "slogan": "Our design is easy to modify and maintain.",
+   "allies": "",
+   "involves": "P",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "34",
+   "area": "Developing",
+   "practice": "Incremental Design and Architecture",
+   "slogan": "We deliver stories every week without compromising design quality.",
+   "allies": "",
+   "involves": "P",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "35",
+   "area": "Developing",
+   "practice": "Spike Solutions",
+   "slogan": "We perform small, isolated experiments when we need more information.",
+   "allies": "",
+   "involves": "P",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "36",
+   "area": "Developing",
+   "practice": "Performance Optimization",
+   "slogan": "We optimize when there's a proven need.",
+   "allies": "",
+   "involves": "cP",
+   "bookHtml": "XXX"
+  },
+  {
+   "id": "37",
+   "area": "Developing",
+   "practice": "Exploratory Testing",
+   "slogan": "We discover surprises and untested conditions",
+   "allies": "",
+   "involves": "T",
+   "bookHtml": "XXX"
+  }
+ ]
+};
+
+
